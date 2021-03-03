ec23afd4bbf16cc2c89f48cca5f4934300db0571
Jose Costa Teixeira <jose.a.teixeira@gmail.com>
2021-03-03 11:05:51 +0100

    Add the automation part

---
+++ setup.cmd
@@ -0,0 +1,9 @@
+npm install -g newman
+npm install async newman-reporter-html newman-reporter-htmlextra 
+
+
+rem npm install newman
+rem npm i async
+rem npm i newman-reporter-html
+rem npm i newman-reporter-htmlextra
+rem npm install newman-reporter-teamcity
